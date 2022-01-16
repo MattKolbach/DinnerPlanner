1100379f075c1f83d1ec1c9f0c9363f98f46e4cc
J.Cole <ccjohn015@gmail.com>
2022-01-16 12:31:41 -0600

    fixes

--- assets/js/script-meal-page.js
+++ assets/js/script-meal-page.js
@@ -64,35 +64,9 @@ const getRandomFoodHandler = function (event) {
     fetch(randomFoodUrl).then(function(response) {
         if (response.ok) {
             //console.log(response);
-            response.json().then(function(data) {
-                console.log(data);
-                const food = data.meals[0]
-                const resultantFoodName = document.createElement("h3");
-                //const resultantFoodArea = document.createElement("p");
-
-                resultantFoodName.textContent = `${food.strMeal}`;
-                //resultantFoodArea.textContent = `${food.strArea}`;
-
-                resultantFoodEl.append(resultantFoodName);
-
-                
-                for (let i = 1; 1 < 100; i++) {
-                  let ingredient = food[`strIngredient${i}`];
-                  let measure = food[`strMeasure${i}`];
-                  if (ingredient) {
-                    console.log(ingredient);
-                    const resultantFoodIngredient = document.createElement("p");
-                    resultantFoodIngredient.textContent = ingredient;
-                    resultantFoodEl.append(resultantFoodIngredient);
-                  }
-                  if (measure) {
-                    console.log(measure);
-                    const resultantFoodMeasure = document.createElement("p");
-                    resultantFoodMeasure.textContent = measure;
-                    resultantFoodEl.append(resultantFoodMeasure);
-                  }
-                }
-            });
+          response.json().then(function(data) {
+               
+          });
         }
     });
 };
